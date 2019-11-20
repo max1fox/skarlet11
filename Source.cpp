253dd2e81aac44a89d471d2d045e1941ce81a015
Max <max1fox@ukr.net>
2019-11-20 23:37:12 +0200

    Added the main .cpp file in whitch describes classes Auto.h and Main.cpp

---
+++ Source.cpp
@@ -0,0 +1,45 @@
+#include "Auto.h"
+int main()
+{
+	Auto A;
+	A.Input();
+	A.Print();
+	A.setPower(143);
+	A.Print();
+	A.setSpeed(260);
+	A.Print();
+	A.setMass(1500);
+	A.Print();
+	A.setLength(3);
+	A.Print();
+	A.setRemoval(1);
+	A.Print();
+	Auto B;
+	B.Input();
+	B.Print();
+	B.setPower(143);
+	B.Print();
+	B.setSpeed(260);
+	B.Print();
+	B.setMass(1500);
+	B.Print();
+	B.setLength(3);
+	B.Print();
+	B.setRemoval(1);
+	B.Print();
+	Auto C;
+	C.Input();
+	C.Print();
+	C.setPower(143);
+	C.Print();
+	C.setSpeed(260);
+	C.Print();
+	C.setMass(1500);
+	C.Print();
+	C.setLength(3);
+	C.Print();
+    C.setRemoval(1);
+	C.Print();
+	system("pause");
+	return 0;
+}
